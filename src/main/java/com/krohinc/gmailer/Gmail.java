d3b7d4c9bda56f0944434f4444d43814c93a7029
Andrew Kroh <andy@crowbird.com>
2014-12-19 19:36:39 -0500

    Adding support for sending MIME message formatted files. Adding RPM packaging.

--- src/main/java/com/krohinc/gmailer/Gmail.java
+++ src/main/java/com/krohinc/gmailer/Gmail.java
@@ -9,7 +9,7 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
- 
+
 import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -22,60 +22,65 @@ import javax.mail.internet.MimeMessage;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
- 
+
 /**
  * Gmail wrapper for sending messages from the command line.
- * 
+ *
  * @version 1.0
  * @author akroh
  */
 public class Gmail {
-    
+
     private static class Arguments
     {
-        @Parameter(names={"--displayname", "-n"}, 
-                   description="Gmail Display Name", 
+        @Parameter(names={"--displayname", "-n"},
+                   description="Gmail Display Name",
                    required=false)
         private String displayName;
 
-        @Parameter(names={"--displayaddress", "-a"}, 
-                   description="Gmail Display Address", 
+        @Parameter(names={"--displayaddress", "-a"},
+                   description="Gmail Display Address",
                    required=false)
         private String displayAddress;
 
-        @Parameter(names={"--username", "-u"}, 
-                   description="Gmail username", 
+        @Parameter(names={"--username", "-u"},
+                   description="Gmail username",
                    required=true)
         private String username;
-        
-        @Parameter(names={"--password", "-p"}, 
-                   description="Gmail password", 
+
+        @Parameter(names={"--password", "-p"},
+                   description="Gmail password",
                    required=true)
         private String password;
-        
-        @Parameter(names={"--subject", "-s"}, 
-                   description="Subject of message", 
-                   required=true)
+
+        @Parameter(names={"--subject", "-s"},
+                   description="Subject of message",
+                   required=false)
         private String subject;
-        
-        @Parameter(names={"--to", "-t"}, 
-                   description="To addresses", 
-                   required=true,
+
+        @Parameter(names={"--to", "-t"},
+                   description="To addresses",
+                   required=false,
                    variableArity = true)
         public List<String> to = new ArrayList<String>();
 
-        @Parameter(names={"--message", "-m"}, 
-                   description="Message text", 
+        @Parameter(names={"--message", "-m"},
+                   description="Message text",
                    required=false)
         private String message;
-        
-        @Parameter(names = { "--messageFile", "-mf" }, 
-                   description = "File to be used as message text", 
+
+        @Parameter(names = { "--messageFile", "-mf" },
+                   description = "File to be used as message text",
                    required = false)
         private String messageFile;
+
+        @Parameter(names = { "--mimeFile", "-f" },
+                description = "File containing a RFC 2047 MIME message.",
+                required = false)
+        private String mimeFile;
     }
-    
-    private static String readFile(String path) throws IOException 
+
+    private static String readFile(String path) throws IOException
     {
         FileInputStream stream = new FileInputStream(new File(path));
         try
@@ -91,83 +96,125 @@ public class Gmail {
             stream.close();
         }
     }
-    
+
     private static void printUsage(JCommander jcommander)
     {
         jcommander.setProgramName("Gmailer");
         jcommander.usage();
-        System.exit(0);
+        System.exit(1);
     }
- 
+
     public static void main(String[] args) throws MessagingException, IOException
     {
         final Arguments arguments = new Arguments();
         JCommander jcommander = new JCommander(arguments);
-        
+
         try
         {
             jcommander.parse(args);
-        } 
+        }
         catch (ParameterException e)
         {
             printUsage(jcommander);
         }
-        
-        if (arguments.message == null && arguments.messageFile == null)
+
+        if (arguments.mimeFile != null)
         {
+            if (arguments.displayAddress != null ||
+                arguments.displayName != null ||
+                arguments.message != null ||
+                arguments.messageFile != null ||
+                arguments.subject != null ||
+                !arguments.to.isEmpty())
+            {
+                System.out.println("--mimeFile (-f) cannot be used with other " +
+                        "message options.");
+                printUsage(jcommander);
+            }
+        }
+        else if (arguments.mimeFile == null &&
+                 arguments.message == null &&
+                 arguments.messageFile == null)
+        {
+            System.out.println("One of --message (-m), --messageFile (-mf), " +
+                    "or --mimeFile (-f) must be specified.");
             printUsage(jcommander);
         }
-        
+        else if (arguments.message != null || arguments.messageFile != null)
+        {
+            if (arguments.to.isEmpty())
+            {
+                System.out.println("--to (-t) must be specified.");
+                printUsage(jcommander);
+            }
+
+            if (arguments.subject == null)
+            {
+                System.out.println("--subject (-s) must be specified.");
+                printUsage(jcommander);
+            }
+        }
+
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
- 
+
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
+                    @Override
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(arguments.username,
                                 arguments.password);
                     }
                 });
- 
-        List<Address> toAddresses = new ArrayList<Address>(arguments.to.size());
-        for (String address : arguments.to)
-        {
-            toAddresses.add(new InternetAddress(address, true));
-        }
-        
-        String fromAddress = arguments.displayAddress == null ?
-            arguments.username : arguments.displayAddress;
 
-        Message message = new MimeMessage(session);
-
-        if (arguments.displayName == null)
+        Message message = null;
+        if (arguments.mimeFile != null)
         {
-            message.setFrom(new InternetAddress(fromAddress));
+            message = new MimeMessage(session,
+                new FileInputStream(arguments.mimeFile));
         }
         else
         {
-            message.setFrom(new InternetAddress(fromAddress,
-                                     arguments.displayName));
-        }
+            message = new MimeMessage(session);
 
-        message.setRecipients(Message.RecipientType.TO, 
-                                  toAddresses.toArray(new Address[]{}));
-        message.setSubject(arguments.subject);
-        
-        if (arguments.messageFile != null)
-        {
-            message.setText(readFile(arguments.messageFile));
-        }
-        else
-        {
-            message.setText(arguments.message);
+            List<Address> toAddresses = new ArrayList<Address>(arguments.to.size());
+            for (String address : arguments.to)
+            {
+                toAddresses.add(new InternetAddress(address, true));
+            }
+
+            String fromAddress = arguments.displayAddress == null ?
+                arguments.username : arguments.displayAddress;
+
+            if (arguments.displayName == null)
+            {
+                message.setFrom(new InternetAddress(fromAddress));
+            }
+            else
+            {
+                message.setFrom(new InternetAddress(fromAddress,
+                        arguments.displayName));
+            }
+
+            message.setRecipients(Message.RecipientType.TO,
+                    toAddresses.toArray(new Address[]{}));
+            message.setSubject(arguments.subject);
+
+            if (arguments.messageFile != null)
+            {
+                message.setText(readFile(arguments.messageFile));
+	        }
+	        else
+	        {
+                message.setText(arguments.message);
+            }
         }
 
         Transport.send(message);
- 
+
         System.out.println("Message sent.");
     }
 }
